a8dcae701c0c2130bd8b0413f61f17e45093077f
im-plumber <5024251030@student.its.ac.id>
2026-09-17 08:17:23 +0700

    add readme and change m1_pesanan.py

---
+++ README.md
@@ -0,0 +1,1 @@
+This is README. Good to make one
--- backend/m1_pesanan.py
+++ backend/m1_pesanan.py
@@ -1,3 +1,11 @@
-print("monkey is animal")
-for x in range(6):
-    pass
+L = []
+L.append(4)
+print(L)
+L.append(2)
+print(L)
+L.append(6)
+print(L)
+# L[1] = ?   nilai 6 di indeks ?
+L.insert(1, -1)
+print(L)
+# L[1] = ?   L[2] = ?
